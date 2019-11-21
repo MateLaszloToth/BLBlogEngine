d3097d7f212f1888bd283fa964346217de68b5fe
Mate Toth <t.mate.laszlo@gmail.com>
2019-11-20 16:53:48 -0800

    Blog and post pages are set up. Refactor classes in html files

--- routes/about.js
+++ routes/about.js
@@ -6,7 +6,7 @@ router.get('/', (req, res)=>{
     //find posts in database and send it to the about page
     db.one('SELECT content FROM HTML WHERE title= $1', 'about')
     .then(about => {
-        console.log("successfully retrieved about article:: " + about.content);
+        console.log("successfully retrieved about article: " + about.content);
         res.render('about/index', { about: about.content});
     })
     .catch(error =>{
@@ -15,15 +15,17 @@ router.get('/', (req, res)=>{
      
 });
 
+//render 'new' page
 router.get('/new', (req, res)=>{
     res.render('about/new');
 });
 
+//Update about content
 router.post('/', (req, res)=>{
-    // update about page
-    const cKEditor = req.body.editor1;
-    console.log( " what up " + cKEditor);
+    const cKEditor = req.body.editor1; //retrieve the content of the editor
+    console.log( "What up?\n" + cKEditor);
     
+    //update database with new content
     db.none('UPDATE HTML SET content = $1 WHERE title = $2',
         [cKEditor, 'about'])
         .then((data) =>{
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,8 +1,46 @@
 const   express = require('express'),
-        router = express.Router();
+        router = express.Router(),
+        db = require('../database/database'),
+        htmlToText = require('html-to-text');
 
 router.get('/', (req, res)=>{
-    res.render('blog/blog');
+    db.any(`SELECT art_id, users.f_name l_name, users.l_name f_name,
+            content, TO_CHAR(created, 'yyyy mm dd') created
+        FROM articles
+        LEFT JOIN users ON articles.author_id = users.user_id   
+        WHERE type = $1 
+        ORDER BY created DESC`, 'blog') // first and last names are swapped because Hungarian names appear in reverse order
+    .then(posts =>{
+        posts.forEach(post => {
+            console.log(post.created);
+            let text = htmlToText.fromString(post.content, {
+                ignoreHref: true,
+                ignoreImage: true
+            });
+            post.intro = text.substring(0, 150);
+
+        });
+        res.render('blog/blog', {posts: posts});
+    })
+    .catch(error =>{
+        console.log(error);
+        res.redirect('back');
+    }); 
+});
+
+router.get('/post/:id', (req, res)=>{
+    db.one(`SELECT art_id, users.f_name l_name, users.l_name f_name,
+            content, TO_CHAR(created, 'yyyy mm dd') created
+        FROM articles
+        LEFT JOIN users ON articles.author_id = users.user_id   
+        WHERE type = $1 AND art_id = $2`, ['blog', req.params.id]) // first and last names are swapped because Hungarian names appear in reverse order
+        .then(post =>{
+            res.render('blog/post', {post: post});
+        })
+        .catch(error =>{
+            console.log(error);
+            res.redirect('back');
+        });
 });
 
 module.exports = router;
